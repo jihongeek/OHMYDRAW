b8017fcb7b93b7b6b44d164f30db5b4024867c0d
jihongeek <jihongeek@kakao.com>
2023-12-15 15:20:04 +0900

    CSS 적용을 위한 선택자 설정 및 CSS 적용

--- web/src/components/ParticipantForm.js
+++ web/src/components/ParticipantForm.js
@@ -1,11 +1,13 @@
+import './ParticipantForm.css'
+
 const ParticipantForm = () => {
     return (
         <div className = "ParticipantForm">
             <p className = "name_label">이름</p>
-            <input type = "text" placeholder = "이름"/>
+            <input className = "name_input" type = "text" placeholder = "이름"/>
             <p className = "name_error_label" style = {{display : "none"}}></p>
             <p className = "email_label">이메일</p>
-            <input type = "text" placeholder = "이메일"/>  
+            <input className = "email_input" type = "text" placeholder = "이메일"/>  
             <p className = "email_error_label" style = {{display : "none"}}></p>
         </div>
     );
