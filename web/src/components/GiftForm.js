cf2ccb012b4e5949d14b436e1b7f748c43ebe6d1
jihongeek <jihongeek@kakao.com>
2023-12-18 15:04:06 +0900

    GiftForm CSS 적용 및 선택자 수정 후 input file 구조 및 수정

--- web/src/components/GiftForm.js
+++ web/src/components/GiftForm.js
@@ -1,11 +1,16 @@
+import './GiftForm.css'
 const GiftForm = () => {
     return (
         <div className = "GiftForm">
             <p className = "gift_name_label">경품 이름</p>
-            <input type = "text" placeholder = "경품 이름"></input>
+            <input className = "gift_name_input" type = "text" placeholder = "경품 이름"></input>
             <p className = "gift_name_error_label" style = {{display : "none"}}></p>
             <p className = "gift_file_label" >경품 파일</p>
-            <input type = "file"/>
+            <input className= "file_name"type = "text" placeholder = "파일 이름" disabled></input>
+            <label for = "file">
+                <div className = "file_upload_button">파일 업로드</div>
+            </label>
+            <input type = "file" id ="file"/>
             <p className = "gift_file_error_label" style = {{display : "none"}}></p>
         </div>
     );
