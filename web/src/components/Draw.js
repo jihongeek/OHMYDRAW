5629fc4b0484f5a23b904e7f0b1731325bbd0786
jihongeek <jihongeek@kakao.com>
2023-12-18 20:50:49 +0900

    Draw 컴포넌트 에 CSS 적용 및 send_gift_response_label 수정

--- web/src/components/Draw.js
+++ web/src/components/Draw.js
@@ -1,7 +1,10 @@
+import "./Draw.css"
 const Draw = () => {
     return (
         <div className = "Draw">
-            <p className = "send_gift_response_label" style ={{display : "none"}}></p>
+            <p className = "send_gift_response_label" style ={{display : "none"}}>경품 발송
+                <a style = {{fontWeight:"bold"}}></a>
+            </p>
             <div className = "winner_wrapper">
                 <p className = "winner_name">당첨자</p>
                 <p className = "win_label">당첨</p>
