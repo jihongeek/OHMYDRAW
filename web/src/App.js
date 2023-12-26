8f26fcd712e907cf4d733b4c0e6744f3a056dec8
jihongeek <jihongeek@kakao.com>
2023-12-26 12:53:32 +0900

    현재 단계 State에 따라 컴포넌트 렌더링 구현,추첨 데이터 설정

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
+import SettingCount from './components/SettingCount';
+import SettingParticipant from './components/SettingParticipant';
+import SettingGift from './components/SettingGift'
+import Draw from './components/Draw'
 
+import {useState} from 'react'
 function App() {
+  const [stepIndex,setStepIndex] = useState(0);
+  const [drawData,setDrawData] = useState({
+    participantCount : 0,
+    winnerCount : 0,
+    participantArray : [],
+    giftArray : []
+});
+  const moveToNextStep = () => {
+    setStepIndex(stepIndex+1);  
+  }
+  const moveToPreviousStep = () => {
+    setStepIndex(stepIndex-1);  
+  }
   return (
     <div className="App">
+      <h1 className="main_label">행운의 추첨</h1>
+      {
+        {
+          0 : <SettingCount moveToNextStep={moveToNextStep}/>,
+          1 : <SettingParticipant moveToNextStep={moveToNextStep} moveToPreviousStep={moveToPreviousStep} />,
+          2 : <SettingGift moveToPreviousStep={moveToPreviousStep}/>,
+          3 : <Draw/>
+        }[stepIndex]
+      }
     </div>
   );
 }
