ed25230fe7e890aeb145e380487a174dfcb156e9
jihongeek <jihongeek@kakao.com>
2024-01-02 12:08:07 +0900

    메인 App 컴포넌트로 부터 추첨 데이터를 불러와서 참가자 수 만큼 폼 생성 및 폼 컴포넌트 인덱스 배열을 만들어서 key로 전달

--- web/src/components/SettingParticipant.js
+++ web/src/components/SettingParticipant.js
@@ -1,7 +1,7 @@
 import ParticipantForm from "./ParticipantForm";
 import './SettingParticipant.css'
-const SettingParticiant = ({moveToNextStep,moveToPreviousStep}) => {
-    const mockProps = [undefined,undefined,undefined,undefined,undefined,undefined];
+const SettingParticiant = ({moveToNextStep,moveToPreviousStep,drawData}) => {
+    const formIndexArray = [...Array(drawData.participantCount)].map((x,index) => index);
     const onClickNextButton = () => { 
         moveToNextStep();
     }
@@ -14,8 +14,8 @@ const SettingParticiant = ({moveToNextStep,moveToPreviousStep}) => {
             <p className="step_label">참가인원 정보</p>
             <div className = "form_wrapper">
                 {
-                    mockProps.map( () => {
-                        return <ParticipantForm/>
+                    formIndexArray.map((index) => {
+                        return <ParticipantForm key={index}/>
                     })
                 }
             </div>
