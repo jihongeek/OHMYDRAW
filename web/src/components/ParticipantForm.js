7376e880ef42024caed4e8fe702bcd5abf9a039d
jihongeek <jihongeek@kakao.com>
2024-01-04 19:25:25 +0900

    참여인원 정보 설정 컴포넌트 유효성 검사 추가

--- web/src/components/ParticipantForm.js
+++ web/src/components/ParticipantForm.js
@@ -1,14 +1,45 @@
 import './ParticipantForm.css'
-
-const ParticipantForm = () => {
+const ParticipantForm = ({nowIndex,errorData,setParticipantData,participantDataArray}) => {
+    const errorMessages = {
+        "name_wrong_character" : "이름은 특수문자를 제외한 영한문자 그리고 숫자만 가능합니다.", 
+        "email_invalid" : "올바르지 않은 이메일 형식입니다."
+    }
+    const onNameInputChange = (e) => {
+        setParticipantData(participantDataArray.map(
+            (participantData,index) => {
+                return index === nowIndex ? {...participantData, name : e.target.value} : participantData
+            }
+        ))
+    }
+    const onEmailInputChange = (e) => {
+        setParticipantData(participantDataArray.map(
+            (participantData,index) => {
+                return index === nowIndex ? {...participantData, email : e.target.value} : participantData
+            }
+        ))
+    }
     return (
         <div className = "ParticipantForm">
             <p className = "name_label">이름</p>
-            <input className = "name_input" type = "text" placeholder = "이름"/>
-            <p className = "name_error_label" style = {{display : "none"}}></p>
+            <input 
+                className = "name_input" 
+                type = "text" 
+                placeholder = "이름" 
+                defaultValue={participantDataArray[nowIndex].name}
+                onChange={onNameInputChange}
+                style={ errorData.nameErrorStatus !== "none" ? {borderColor : "#E63535"} : null }
+            />
+            {  errorData.nameErrorStatus !== "none" && <p className = "name_error_label">{errorMessages[errorData.nameErrorStatus]}</p>}
             <p className = "email_label">이메일</p>
-            <input className = "email_input" type = "text" placeholder = "이메일"/>  
-            <p className = "email_error_label" style = {{display : "none"}}></p>
+            <input 
+                className = "email_input" 
+                type = "text" 
+                placeholder = "이메일" 
+                defaultValue={participantDataArray[nowIndex].email}
+                onChange={onEmailInputChange}
+                style={ errorData.emailErrorStatus !== "none" ? {borderColor : "#E63535"} : null }
+            />  
+            {  errorData.emailErrorStatus !== "none" && <p className = "email_error_label">{errorMessages[errorData.emailErrorStatus]}</p>}
         </div>
     );
 }
--- web/src/components/SettingParticipant.js
+++ web/src/components/SettingParticipant.js
@@ -1,8 +1,41 @@
 import ParticipantForm from "./ParticipantForm";
-import './SettingParticipant.css'
+import './SettingParticipant.css';
+import {useState} from 'react';
+
 const SettingParticiant = ({moveToNextStep,moveToPreviousStep,drawData}) => {
+    
     const formIndexArray = [...Array(drawData.participantCount)].map((x,index) => index);
+    const [errorDataArray,setErrorData] = useState(formIndexArray.map(x => { return {nameErrorStatus : "none", emailErrorStatus : "none"} }));
+    const [participantDataArray,setParticipantData] = useState(formIndexArray.map(x => { return { name : "", email : ""} }))
     const onClickNextButton = () => { 
+        const nameValidationRegex = /^([a-zA-Zㄱ-ㅎ가-힣 0-9]{1,})$/;
+        const emailValidationRegex = /[a-zA-Z0-9][a-zA-Z0-9_-]{0,20}@[a-zA-Z0-9]{1,}\.[a-zA-Z0-9]{1,}/
+        let isErrorOccured = false
+        setErrorData(errorDataArray.map((errorData, index) => {
+            let errorDataToUpdate = {
+                nameErrorStatus : "none",
+                emailErrorStatus : "none"
+            }
+            if (nameValidationRegex.test(participantDataArray[index].name) === false)
+            {
+                isErrorOccured = true;
+                errorDataToUpdate.nameErrorStatus = "name_wrong_character";
+            }
+            if (emailValidationRegex.test(participantDataArray[index].email) === false)
+            {
+                isErrorOccured = true;
+                errorDataToUpdate.emailErrorStatus = "email_invalid";
+            }
+            if (isErrorOccured === true) 
+            {
+                return errorDataToUpdate
+            }
+            return {emailErrorStatus : "none" ,nameErrorStatus : "none"}
+        }));
+        if (isErrorOccured === true)
+        {
+            return;
+        }
         moveToNextStep();
     }
     const onClickPreviousButton = () => {
@@ -14,8 +47,13 @@ const SettingParticiant = ({moveToNextStep,moveToPreviousStep,drawData}) => {
             <p className="step_label">참가인원 정보</p>
             <div className = "form_wrapper">
                 {
-                    formIndexArray.map((index) => {
-                        return <ParticipantForm key={index}/>
+                    participantDataArray.map((x,index) => {
+                        return <ParticipantForm 
+                        errorData={errorDataArray[index]}
+                        participantDataArray = {participantDataArray} 
+                        setParticipantData = {setParticipantData}  
+                        nowIndex = {index} key={index}
+                        />
                     })
                 }
             </div>
