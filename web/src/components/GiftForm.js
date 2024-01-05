8061a0884d6b59c40493d81c9ab3a222ddaaf197
jihongeek <jihongeek@kakao.com>
2024-01-05 18:13:58 +0900

    GiftForm 컴포넌트의 label 태그 for 속성을 리액트에 맞게 htmlFor로 수정

--- web/src/components/GiftForm.js
+++ web/src/components/GiftForm.js
@@ -7,7 +7,7 @@ const GiftForm = () => {
             <p className = "gift_name_error_label" style = {{display : "none"}}></p>
             <p className = "gift_file_label" >경품 파일</p>
             <input className= "file_name"type = "text" placeholder = "파일 이름" disabled></input>
-            <label for = "file">
+            <label htmlFor = "file">
                 <div className = "file_upload_button">파일 업로드</div>
             </label>
             <input type = "file" id ="file"/>
