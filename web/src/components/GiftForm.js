0fe1cb39fe17e8fd9ff1e7176cefd91fcb859256
jihongeek <jihongeek@kakao.com>
2024-01-22 21:48:08 +0900

    경품 업로드 유효성 검사 구현 완료

--- web/src/components/GiftForm.js
+++ web/src/components/GiftForm.js
@@ -1,5 +1,10 @@
 import './GiftForm.css'
-const GiftForm = ({giftDataArray,setGiftData,nowIndex}) => {
+const GiftForm = ({giftDataArray,errorData,setGiftData,nowIndex}) => {
+    const errorMessages = {
+        "name_wrong_character" : "경품명은 특수문자를 제외한 영한문자 그리고 숫자만 가능합니다.", 
+        "no_file" : "파일을 업로드 해주세요",
+        "invalid_file" : "1mb 이하의 jpg, png 파일만 업로드 가능합니다."
+    }
     const onGiftFileChange = (e) => {
         setGiftData(giftDataArray.map((giftData,index)=>{
             if (index === nowIndex && e.target.files.length > 0)
@@ -23,13 +28,16 @@ const GiftForm = ({giftDataArray,setGiftData,nowIndex}) => {
                 placeholder = "경품 이름"
                 defaultValue={giftDataArray[nowIndex].giftName}
                 onChange={onGiftNameChange}      
+                style={ errorData.nameErrorStatus !== "none" ? {borderColor : "#E63535"} : null }
             />
+            {  errorData.nameErrorStatus !== "none" && <p className = "gift_name_error_label">{errorMessages[errorData.nameErrorStatus]}</p>}
             <p className = "gift_name_error_label" style = {{display : "none"}}></p>
             <p className = "gift_file_label" >경품 파일</p>
             <input 
                 className= "file_name" 
                 type = "text" 
                 placeholder={giftDataArray[nowIndex].giftFile.name}
+                style={ errorData.fileErrorStatus !== "none" ? {borderColor : "#E63535"} : null }
                 disabled
             />
             <label htmlFor = "file">
@@ -41,7 +49,7 @@ const GiftForm = ({giftDataArray,setGiftData,nowIndex}) => {
                 accept="image/png, image/jpeg"
                 onChange={onGiftFileChange}
             />
-            <p className = "gift_file_error_label" style = {{display : "none"}}></p>
+            {  errorData.fileErrorStatus !== "none" && <p className = "gift_file_error_label">{errorMessages[errorData.fileErrorStatus]}</p>}
         </div>
     );
 }
--- web/src/components/SettingGift.js
+++ web/src/components/SettingGift.js
@@ -6,8 +6,60 @@ const SettingGift = ({moveToPreviousStep,drawData}) => {
     const [giftDataArray,setGiftData] = useState([...Array(drawData.winnerCount)].map(
         (_,index) => { return { id : index, giftName : "", giftFile : new File([],"파일 이름")} }
     ));
+    const [errorDataArray,setErrorData] = useState(giftDataArray.map(x => { return {nameErrorStatus : "none", fileErrorStatus : "none"}}));    
+    
+    const maxNonNamePartLength = 4;
+    const isValidImageFilename = (filename) => {
+        const filenameValidationRegex = /\.JPG$|\.jpg$|\.jpeg$|\.JPEG$|\.png$|\.PNG$/
+        if (filename.length < maxNonNamePartLength)
+        {
+            return false;
+        }
+        if (filenameValidationRegex.test(filename) === false)
+        {
+            return false;
+        }
+        return true;
+    }
     const onClickPreviousButton = () => {
         moveToPreviousStep();
+    }
+    const onClickDrawButton = () => {
+        const nameValidationRegex = /^([a-zA-Zㄱ-ㅎ가-힣 0-9]{1,})$/;
+        setErrorData(errorDataArray.map((errorData, index)=> {
+            let isErrorOccured = false;
+            let errorDataToUpdate = {
+                nameErrorStatus : "none",
+                fileErrorStatus : "none"
+            }
+            if (nameValidationRegex.test(giftDataArray[index].giftName) === false)
+            {
+                isErrorOccured = true;
+                errorDataToUpdate.nameErrorStatus = "name_wrong_character";
+            }
+            if (isValidImageFilename(giftDataArray[index].giftFile.name) === false)
+            {
+                isErrorOccured = true;
+                errorDataToUpdate.fileErrorStatus = "invalid_file";
+            }
+            if (giftDataArray[index].giftFile.size > 1000000)
+            {
+                isErrorOccured = true;
+                errorDataToUpdate.fileErrorStatus = "invalid_file";
+            }
+            if (giftDataArray[index].giftFile.size === 0 && giftDataArray[index].giftFile.name === "파일 이름")
+            {
+                isErrorOccured = true;
+                errorDataToUpdate.fileErrorStatus = "no_file";
+            }
+            if (isErrorOccured === true) 
+            {
+                return errorDataToUpdate
+            }
+            return {fileErrorStatus : "none" ,nameErrorStatus : "none"}
+        
+        }))
+        
     }
     return ( 
         <div className = "SettingGift">
@@ -21,13 +73,14 @@ const SettingGift = ({moveToPreviousStep,drawData}) => {
                             giftDataArray = {giftDataArray}
                             setGiftData = {setGiftData}
                             nowIndex = {giftData.id}
+                            errorData={errorDataArray[index]}
                         />
                     })
                 }
             </div>
             <div className ="button_wrapper">
                 <button className = "previous_button" onClick = {onClickPreviousButton}>이전</button>
-                <button className = "start_draw_button">추첨하기</button>
+                <button className = "start_draw_button" onClick={onClickDrawButton}>추첨하기</button>
             </div>
         </div>
     );
