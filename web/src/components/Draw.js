8b63c6c7c5b9ab6a641625535978126475b2061c
jihongeek <jihongeek@kakao.com>
2024-02-06 15:23:44 +0900

    추첨 데이터 표시 및 당첨자 렌더링, 버튼 이벤트 적용 해놓기

--- web/src/components/Draw.js
+++ web/src/components/Draw.js
@@ -1,16 +1,16 @@
 import "./Draw.css"
-const Draw = () => {
+const Draw = ({drawData,doDraw}) => {
     return (
         <div className = "Draw">
             <p className = "send_gift_response_label" style ={{display : "none"}}>경품 발송
-                <a style = {{fontWeight:"bold"}}></a>
+                <a style = {{fontWeight:"bold"}}>{}</a>
             </p>
             <div className = "winner_wrapper">
-                <p className = "winner_name">당첨자</p>
+                <p className = "winner_name">{drawData.participantArray[drawData.nowWinnerId].name}</p>
                 <p className = "win_label">당첨</p>
             </div>
             <div className = "button_wraper">
-                <button className="redraw_button">다시 추첨</button>
+                <button className="redraw_button" onClick={ () => doDraw(true) }>다시 추첨</button>
                 <button className="send_gift_button">경품 발송</button>
             </div>
         </div>
