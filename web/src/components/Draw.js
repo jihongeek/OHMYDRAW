58b429536e836b8339548402c7111557701ec69f
jihongeek <jihongeek@kakao.com>
2024-02-22 15:33:29 +0900

    Feat : Draw 화면 및 로직 구현

--- web/src/components/Draw.js
+++ web/src/components/Draw.js
@@ -1,17 +1,66 @@
-import "./Draw.css"
-const Draw = ({drawData,doDraw}) => {
+import "./Draw.css";
+import { useEffect, useState } from "react";
+const Draw = ({drawData,doDraw,setDrawData}) => {
+    const [sendStatus,setSendStatus] = useState('ready');
+    const nowParticipantArray = drawData.participantArray.filter(
+        (participantData) => participantData.isWon === false);
+    const sendStatusMessages = {
+        'ready' : {text : '준비완료', color : 'inherit' },
+        'sending' : {text : '하는 중', color : 'inherit' },
+        'success' : {text : '성공', color : '#4DF472'},
+        'failed' : {text : '실패', color : '#E63535'} 
+    }
+    const onClickSendGiftButton = ()=>{
+        setSendStatus('sending');
+        fetch("sent-gifts",{method : "POST"})
+            .then((response) => {
+                if (response.status === 201)
+                {
+                    setSendStatus('success');   
+                }
+                else
+                {
+                    setSendStatus('failed');
+                }           
+            })
+    }
+    const onClickDrawNextButton = ()=>{
+        setSendStatus('ready');
+        doDraw(false,nowParticipantArray);
+    }
+    useEffect(()=>{
+        if (sendStatus === 'success')
+        {
+            const nextGiftArray = [...drawData.giftArray];
+            const nextParticipantArray= drawData.participantArray.map((participantData,index)=>{
+                if (index === drawData.nowWinnerId)
+                {
+                    return {...participantData,isWon : true}
+                }
+                return participantData
+            })
+            nextGiftArray.shift() 
+            nowParticipantArray.splice(drawData.pickedIndex,1);
+            setDrawData({...drawData,giftArray : nextGiftArray});
+            setDrawData({...drawData,participantArray : nextParticipantArray});
+        }
+    },
+    [sendStatus])
     return (
         <div className = "Draw">
-            <p className = "send_gift_response_label" style ={{display : "none"}}>경품 발송
-                <a style = {{fontWeight:"bold"}}>{}</a>
+            <p className = "send_gift_response_label">경품 발송
+                <span style = {{fontWeight:"bold", color : sendStatusMessages[sendStatus].color}}>
+                    {"  " + sendStatusMessages[sendStatus].text}
+                </span>
             </p>
             <div className = "winner_wrapper">
                 <p className = "winner_name">{drawData.participantArray[drawData.nowWinnerId].name}</p>
                 <p className = "win_label">당첨</p>
             </div>
             <div className = "button_wraper">
-                <button className="redraw_button" onClick={ () => doDraw(true) }>다시 추첨</button>
-                <button className="send_gift_button">경품 발송</button>
+                {sendStatus === 'success' || (sendStatus === 'ready' && nowParticipantArray.length < 2) ? '' : <button className="redraw_button" onClick={ () => doDraw(true,nowParticipantArray) }>다시 추첨</button>}
+                {sendStatus === 'success' ? '' : <button className="send_gift_button" onClick={onClickSendGiftButton}>경품 발송</button>}
+                {sendStatus === 'success' && nowParticipantArray.length > 0 && drawData.giftArray.length > 0  ? <button className="draw_next_button" onClick={onClickDrawNextButton}>다음 추첨</button> : ''}
             </div>
         </div>
     );
