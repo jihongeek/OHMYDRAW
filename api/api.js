0b6afc9f5e0614e4f092ad18b062ac93e1077b61
jihongeek <jihongeek@kakao.com>
2024-03-03 21:54:49 +0900

    feat : 경품 전송 API 구현

---
+++ api/api.js
@@ -0,0 +1,38 @@
+import {requestToSendEmail} from './email.js'
+import express from 'express';
+import cors from 'cors';
+const { EMAIL_PASSWORD, EMAIL_USERNAME,EMAIL_HOST } = process.env;
+const app = express();
+const port = 3001;
+
+const senderData = {
+        host : EMAIL_HOST,
+        port : 465,
+        secure : true,
+        auth : {
+            user : EMAIL_USERNAME,
+            pass : EMAIL_PASSWORD 
+    }
+}
+app.use(express.json())
+app.use(cors())
+app.post('/sent-gifts', (req,res) => {
+    console.log(req.body);
+    const message = {
+        from: EMAIL_USERNAME,
+        to: req.body['winnerEmail'],
+        subject: `축하합니다! ${req.body['winnerName']}님 당첨되셨습니다!`,
+    }
+    if (requestToSendEmail(senderData,message))
+    {
+        res.status(201).send();
+    }
+    else
+    {
+        res.status(400).send();
+    }
+});
+
+app.listen(port, ()=> {
+    console.log(`${port}에서 돌아가는 앱`);
+})
