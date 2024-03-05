9cb6152182cde99283ee911303ef63a3b107fb90
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2024-03-05 06:35:19 +0000

    feat : API 이메일 전송 오류 처리 구현

--- api/api.js
+++ api/api.js
@@ -17,21 +17,20 @@ const senderData = {
 app.use(express.json())
 app.use(cors())
 app.post('/sent-gifts', (req,res) => {
-    console.log(req.body);
     const message = {
         from: EMAIL_USERNAME,
         to: req.body['winnerEmail'],
         subject: `축하합니다! ${req.body['winnerName']}님 당첨되셨습니다!`,
         attachments : [{path : req.body['giftFile']}]
     }
-    if (requestToSendEmail(senderData,message))
-    {
+    requestToSendEmail(senderData,message).then(()=>{
         res.status(201).send();
-    }
-    else
-    {
-        res.status(400).send();
-    }
+    })
+    .catch((error)=>{
+        console.log(error);
+        res.status(400).send()
+    })
+    
 });
 
 app.listen(port, ()=> {
--- api/email.js
+++ api/email.js
@@ -1,16 +1,10 @@
 import nodemailer from 'nodemailer'
 
 // 매개변수에 대해 더 생각해봐야 할 듯
-function requestToSendEmail(senderData,message) {
+async function requestToSendEmail(senderData,message) {
     let transporter = nodemailer.createTransport(senderData);
-    transporter.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        transporter.sendMail(message);    
-    }
-    });
-    return true;
+    await transporter.verify();
+    return transporter.sendMail(message);
 }
 
 export {requestToSendEmail};
