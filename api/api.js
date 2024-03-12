c37ae4f99689d3f8f354a70ab02c3328655bd79e
Jihong Kim <34394165+jihongeek@users.noreply.github.com>
2024-03-12 12:47:00 +0000

    chore : 용량 제한 추가 및 환경변수 파일 경로 재설정 및 시작 로그 변경

--- api/api.js
+++ api/api.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({path : './api/.env'});
 const { EMAIL_PASSWORD, EMAIL_USERNAME,EMAIL_HOST } = process.env;
 const app = express();
 const port = 3001;
@@ -17,8 +17,13 @@ const senderData = {
 
     }
 }
-app.use(express.json())
+app.use(express.json({limit: '2mb'}))
+app.use(express.urlencoded({
+  limit: '2mb',
+  extended: false
+}))
 app.use(cors())
+app.use(express.json());
 app.post('/sent-gifts', (req,res) => {
     const message = {
         from: EMAIL_USERNAME,
@@ -37,5 +42,5 @@ app.post('/sent-gifts', (req,res) => {
 });
 
 app.listen(port, ()=> {
-    console.log(`${port}에서 돌아가는 앱`);
+    console.log(`${port}포트에서 API 서버가 정상적으로 가동 중입니다.`);
 })
